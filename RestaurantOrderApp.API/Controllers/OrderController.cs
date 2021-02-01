5769ad1eb015a74a39971fd4bb0202e461dcc0bb
Ulysses Queiroz <ulyssesq@ganhejunto.com.br>
2021-02-01 02:47:52 -0300

    Adding cors

--- RestaurantOrderApp.API/Controllers/OrderController.cs
+++ RestaurantOrderApp.API/Controllers/OrderController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using RestaurantOrderApp.Domain.Entities;
+using RestaurantOrderApp.Domain.Interfaces.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,16 +15,21 @@ namespace RestaurantOrderApp.API.Controllers
     public class OrderController : ControllerBase
     {
         private readonly ILogger<OrderController> _logger;
+        private readonly IOrderService _orderService;
 
-        public OrderController(ILogger<OrderController> logger)
+        public OrderController(ILogger<OrderController> logger, IOrderService orderService)
         {
             _logger = logger;
+            _orderService = orderService;
+
         }
 
-        [HttpGet]
-        public Order Get(string input)
+        
+        [HttpGet("{input}")]
+        [EnableCors("AllowAll")]
+        public IActionResult Get(string input)
         {
-            return new Order();
+            return Ok(_orderService.Get(input));
         }
     }
 }
--- RestaurantOrderApp.API/Startup.cs
+++ RestaurantOrderApp.API/Startup.cs
@@ -28,7 +28,18 @@ namespace RestaurantOrderApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder => builder.AllowAnyOrigin()
+                                      .AllowAnyMethod()
+                                      .AllowAnyHeader());
+            });
+
             services.AddControllers();
+
+            
 
             DependencyInjectionConfig.Configure(services);
             //services.AddTransient<IValidationService, ValidationService>()
@@ -46,6 +57,8 @@ namespace RestaurantOrderApp.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
